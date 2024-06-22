17c209d211e3efa71dd80be9466cc5c0b585e41a
mourad denguir <denguirmourad0@gmail.com>
2024-06-22 09:45:33 +0100

    test

--- back-end/game/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from . import consumer
-
-websocket_urlpatterns = [
-    re_path(r"ws/gamematch/(?P<game_id>\w+)/$",consumer.GameConsumer.as_asgi()),
-    re_path(r"ws/lobby/(?P<lobby_id>\w+)/$",consumer.LobbyConsumer.as_asgi()),
-]
