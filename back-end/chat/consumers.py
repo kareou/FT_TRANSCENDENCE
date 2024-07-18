196c0e5377f5dbbac6707d0dfc9330a4133dca47
DENGUIR Mourad <denguirmourad0@gmail.com>
2024-07-18 18:39:39 +0100

    commit fixing the websockets private rooms logic

--- back-end/chat/consumers.py
+++ back-end/chat/consumers.py
@@ -1,18 +1,26 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from django.contrib.auth.models import AnonymousUser
+from .models import Conversation, ChatMessage
+from django.db.models import Q
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.user_id = self.scope['url_route']['kwargs']['user_id']
-        self.receiver_id = self.scope['url_route']['kwargs']['receiver_id']
+        print(f"Connecting .......")
+        self.user_id = int(self.scope['url_route']['kwargs']['user_id'])
+        self.receiver_id = int(self.scope['url_route']['kwargs']['receiver_id'])
         self.room_name = f"private_chat_{min(self.user_id, self.receiver_id)}_{max(self.user_id, self.receiver_id)}"
         self.room_group_name = f"chat_{self.room_name}"
-        print(f"chat group name =  {self.room_group_name}")
+        
+        print(f"Chat group name = {self.room_group_name}")
+        print(f"User ID: {self.user_id}, Receiver ID: {self.receiver_id}")
+
+
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
-
         await self.accept()
         print(f"WebSocket connection established for {self.channel_name}")
 
@@ -30,10 +38,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
             sender = text_data_json['sender']
             print(f"Received message: {message}")
 
+            if int(sender) not in [self.user_id, self.receiver_id]:
+                print(f"User {sender} is not part of the conversation.")
+                await self.send(text_data=json.dumps({
+                    'error': 'User not part of the conversation'
+                }))
+                return
+
             await self.channel_layer.group_send(
                 self.room_group_name,
                 {
-                    'type': 'chat_message',
+                    'type': 'send_message',
                     'sender': sender,
                     'message': message
                 }
@@ -44,14 +59,19 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 'error': 'Invalid message format'
             }))
 
-    async def chat_message(self, event):
+    async def send_message(self, event):
         message = event['message']
         sender = event['sender']
         print(f"Sending message to WebSocket: {message}")
+
+        if int(sender) not in [self.user_id, self.receiver_id]:
+            print(f"User {sender} is not part of the conversation.")
+            await self.send(text_data=json.dumps({
+                'error': 'User not part of the conversation'
+            }))
+            return
 
         await self.send(text_data=json.dumps({
             'message': message,
             'sender': sender
         }))
-
-
--- back-end/start.sh
+++ back-end/start.sh
@@ -1,4 +1,5 @@
-#!bin/bash
+#!/bin/bash
 
 python manage.py migrate
+
 python manage.py runserver 0.0.0.0:8000
