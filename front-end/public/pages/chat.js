1c950a4276b727d7fb4a7fa148ea91b8ced20038
Guennach Othmane <gothmane@c1r3p4.1337.ma>
2024-08-27 17:57:36 +0100

    updating some styling

--- front-end/public/pages/chat.js
+++ front-end/public/pages/chat.js
@@ -44,6 +44,11 @@ export default class Chat extends HTMLElement {
         return `${dayName}, ${month}/${day}/${year}`;
     }
 
+    escapeHTML(html) {
+        return html.replace(/&/g, '&amp;')
+                   .replace(/</g, '&lt;')
+                   .replace(/>/g, '&gt;');
+    }
 
     setupWebSocket() {
         // console.log("size = " +this.conversations.length);
@@ -113,7 +118,6 @@ export default class Chat extends HTMLElement {
         <div class="first_profile_wrapper_chat">
             <div class="pdp_warpper">
                 <img src="http://localhost:8000${this.user.profile_pic}" class="pfp_logo" alt="profile picture" loading="lazy">
-                <div class="txt_one">Chats</div>
             </div>
             <div class="icon_wrapper_">
                 <i class="fa-solid fa-circle-ellipsis fa-2xl" style="color: #ffffff;"></i>
@@ -126,7 +130,7 @@ export default class Chat extends HTMLElement {
             <div class="chat_bulles_wrapper user" id="${user.id}" name="${user.username}">
                 <div class="chat_bulle_wrapper">
                     <div class="chat_pdp_wrapper" style="position: relative;">
-                        <user-avatar image="${user.profile_pic}" state="online" width="70" height="70"></user-avatar>
+                        <user-avatar image="${user.profile_pic}" state="online" width="50" height="50"></user-avatar>
                     </div>
                     <div class="data_chat_bulle_wrapper">
                         <div class="name_bulle_chat">${user.username}</div>
@@ -148,12 +152,12 @@ export default class Chat extends HTMLElement {
         userElements.forEach(userElement => {
             const receiverId = userElement.id;
             const senderId = this.user.id;
-            this.receiverId = receiverId;
             const websocket = new WebSocket(`ws://127.0.0.1:8000/ws/chat/${senderId}/${receiverId}/`)
             const newConversation = {senderId, receiverId, websocket};
             this.conversations.push(newConversation);
             
             userElement.addEventListener('click', () => {
+                this.receiverId = receiverId;
                 this.querySelector('.blocked-user-message').style.display = 'none';
                 this.querySelector('.input_conv_container__chat').style.display = 'flex';
 
@@ -315,42 +319,54 @@ export default class Chat extends HTMLElement {
     displayNewMessage(data)
     {
         let messageHTML = '';
-        
+        const message_time =  this.isToday(data.timestamp) ? this.formatTime(data.timestamp) : this.formatDate(data.timestamp);
         if (data.sender != this.user.id)
         {
             messageHTML = `
-                <div class="slot_message_container___">
-                    <div class="message_container__ second_msg_user">
-                        <div class="message_user_container__ right_side_msg">
-                            <div class="message_user_content__">
-                                ${data.message}
-                            </div>
+            <div class="slot_message_container___ ">
+            <div class="message_container__ second_msg_user ">
+                <div class='content_time_msg'>
+                    <div class="message_user_container__ right_side_msg">
+                        <div class="message_user_content__">
+                            ${data.message}
                         </div>
                     </div>
+                    <div class='msg_time'>
+                        ${message_time}
+                    </div >
                 </div>
-            `;
+                <img src="${this.querySelector(".logo_chat_user").src}" class="user_pic_msg_right" alt="profile picture" loading="lazy">
+            </div>
+        </div>
+    `;
         }
         else
         {
             messageHTML = `
-                <div class="slot_message_container___">
-                    <div class="message_container__ first_msg_user">
-                        <div class="message_user_container__">
+            <div class="slot_message_container___">
+            <div class="message_container__ first_msg_user">
+            <img src="http://localhost:8000${this.user.profile_pic}" class="user_pic_msg_left" alt="profile picture" loading="lazy">
+                    <div class="message_user_container__">
+                        <div class='content_time_msg'>
                             <div class="message_user_content__">
                                 ${data.message}
                             </div>
                         </div>
+                        <div class='msg_time'>
+                        ${message_time}
+                        </div>
                     </div>
                 </div>
-            `;
+            </div>
+        `;
         }
         const userElements = this.querySelectorAll('.chat_bulles_wrapper');
-        console.log("log iser : " +this.user.id);
+        // console.log("log iser : " +this.user.id);
         userElements.forEach(userElement => {
-            console.log("id : " +userElement.getAttribute("id")+ " rec : " + data.sender);
+            // console.log("id : " +userElement.getAttribute("id")+ " rec : " + data.sender);
             if (userElement.getAttribute("id") == data.sender)
             {
-                console.log("test " +userElement.getAttribute("id"));
+                // console.log("test " +userElement.getAttribute("id"));
                 userElement.querySelector('.last_msg').innerHTML = data.message;
             }
         })
@@ -444,7 +460,7 @@ export default class Chat extends HTMLElement {
             return;
         }
 
-        const messageContent = document.querySelector('.message-input').value;
+        const messageContent = this.escapeHTML(document.querySelector('.message-input').value);
         if (messageContent.trim().length != 0)
         {
             const messageData = {
@@ -480,11 +496,13 @@ export default class Chat extends HTMLElement {
                         type: 'message',
                         message: data.message,
                         sender: data.sender,
+                        timestamp: data.timestamp
                     };
                     this.conversations.forEach(conversation => {
-                    if ((conversation.senderId == this.user.id && conversation.receiverId == this.receiverId) || (conversation.senderId == this.receiverId && conversation.receiverId == this.user.id))
+                    if ((conversation.senderId == this.user.id && conversation.receiverId == this.receiverId))
                     {
                         conversation.websocket.send(JSON.stringify(dataWs));
+                        console.log('sender : ', this.user.id, ' receiver : ', this.receiverId);
                         console.log('Message sent via ws:', dataWs);
                         document.querySelector('.message-input').value = '';
                     }
@@ -618,7 +636,7 @@ export default class Chat extends HTMLElement {
 
                     <h1>select a conversation to start chatting</h1>
                     <p>or find a friend and chat</p>
-                    <button class="find_friends">Find Friends <i class="fa-light fa-user-group"></i> </button>
+                    <button class="find_friends"><span class="find_friends_span">Find Friends <i class="fa-light fa-user-group"></span></i> </button>
             </div>
             <div class="second_section_wrapper_chat">
                   <div class="wrapper_first_con_second_section">
