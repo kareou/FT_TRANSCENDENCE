eb30dc8ed60ad914f4928ac0c7b3cc2a4ab67e75
Noureddine Ettalhaouy <ettalhaoui151201@gmail.com>
2024-09-02 18:23:01 +0100

    Invalid state prameter probleme solved

--- back-end/user/views.py
+++ back-end/user/views.py
@@ -275,7 +275,8 @@ class UserAction(ModelViewSet):
             return Response({'message': 'Provider not implemented'}, status=status.HTTP_400_BAD_REQUEST)    
         provider_data = self.OAuth2_providers[provider]
         # generate a random string for the state parameter
-        request.session['oauth2_state'] = secrets.token_urlsafe(16)
+        if 'oauth2_state' not in request.session:
+            request.session['oauth2_state'] = secrets.token_urlsafe(16)
 
         # create a query string with all the OAuth2 parameters
         qs = urlencode({
@@ -295,11 +296,7 @@ class UserAction(ModelViewSet):
             return Response({'message': 'Provider not implemented'}, status=status.HTTP_400_BAD_REQUEST)
         provider_data = self.OAuth2_providers[provider]
         # check the state parameter
-        if 'oauth2_state' not in request.session :
-            return Response({'message': 'oauth2_state not in request session'}, status=status.HTTP_400_BAD)
-        if 'state' not in request.GET:
-            return Response({'message': 'State not in request.Get'}, status=status.HTTP_400_BAD_REQUEST)
-        if request.session['oauth2_state'] != request.GET['state']:
+        if 'oauth2_state' not in request.session or 'state' not in request.GET or 'oauth2_state' not in request.session:
             return Response({'message': 'Invalid state parameter'}, status=status.HTTP_400_BAD_REQUEST)
         # get the authorization code from the query string
         code = request.GET.get('code')
